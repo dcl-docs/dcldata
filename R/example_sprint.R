25bf92e3b89160037f9c6519d59b59db3592ac1a
skaltman <skaltman@stanford.edu>
2019-12-12 13:15:43 -0800

    add sprint data from data-vis

---
+++ R/example_sprint.R
@@ -0,0 +1,12 @@
+#' Example men's 100m dash data 
+#'
+#' Results of the men's 100-meter dash final from the 2016 Summer Olympics
+#'
+#' @format A data frame with 11 rows and 2 variables.
+#' \describe{
+#'   \item{x_1, y}{Point coordinates}
+#' }
+#'
+#' @source Wikipedia.
+#' \url{https://en.wikipedia.org/wiki/Athletics_at_the_2016_Summer_Olympics_%E2%80%93_Men%27s_100_metres#Final}
+"example_sprint"
