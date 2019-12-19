f5149acbd269198d768ccdf5813c665b4ad09b95
skaltman <skaltman@stanford.edu>
2019-12-18 17:57:47 -0800

    add buildings data

---
+++ R/buildings.R
@@ -0,0 +1,17 @@
+#' Building height data
+#'
+#' Heights and other data on the world's 71 tallest buildings.
+#'
+#' @format A data frame with 71 rows and 6 variables.
+#' \describe{
+#'   \item{building}{Name of the building}
+#'   \item{city}{Home city of the building}
+#'   \item{country}{Home country of the building}
+#'   \item{height}{Height of the building, in feet}
+#'   \item{floors}{Number of floors}
+#'   \item{year}{Year the building was built}
+#' }
+#'
+#' @source Wikipedia. 
+#' \url{https://en.wikipedia.org/wiki/List_of_tallest_buildings#Tallest_buildings_in_the_world}
+"buildings"
