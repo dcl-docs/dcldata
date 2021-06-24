f8d3aeb0646e92decc241e6235c71ecd37bfe6fa
Bill Behrman <behrman@stanford.edu>
2021-06-24 13:26:47 -0700

    Update buildings

--- data-raw/buildings/buildings.R
+++ data-raw/buildings/buildings.R
@@ -1,7 +1,7 @@
 # Building height data
 
 # Author: Sara Altman, Bill Behrman
-# Version: 2019-12-18
+# Version: 2021-06-24
 
 # Libraries
 library(tidyverse)
@@ -9,10 +9,9 @@ library(rvest)
 
 # Parameters
   # URL 
-url_data <-
-  "https://en.wikipedia.org/wiki/List_of_tallest_buildings"
+url_data <- "https://en.wikipedia.org/wiki/List_of_tallest_buildings"
   # CSS selector
-css_selector <- "#mw-content-text > div > table:nth-child(22)"
+css_selector <- "#mw-content-text > div > table:nth-child(23)"
 
 #===============================================================================
 
@@ -21,15 +20,10 @@ buildings <-
   read_html() %>%
   html_node(css = css_selector) %>%
   html_table(fill = TRUE) %>%
-  as_tibble(.name_repair = "unique") %>% 
   janitor::row_to_names(row_number = 1) %>% 
-  rename_all(str_to_lower) %>% 
-  rename(height = `height[9]`, building = name) %>% 
-  mutate(
-    height = str_remove_all(height, "[:punct:]") %>% as.double(),
-    floors = as.double(floors),
-    year = as.integer(year)
-  ) %>% 
+  rename_with(str_to_lower) %>% 
+  rename(building = name, height = ft) %>% 
+  mutate(across(c(height, floors, year), parse_number)) %>% 
   select(building, city, country, height, floors, year)
 
 usethis::use_data(buildings, overwrite = TRUE)
